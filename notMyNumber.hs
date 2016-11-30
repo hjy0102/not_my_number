ed7a3d0e4b9075f3d07a99390a953f32ae397e3a
Hyojin Yi <hyojinyi@live.ca>
2016-11-29 22:27:49 -0800

    missing a few functions so errors

--- notMyNumber.hs
+++ notMyNumber.hs
@@ -8,8 +8,8 @@ module NotMyNumber where
 -- :load NotMyNumber
 
 import System.IO 
-import Player
-import Game
+-- import Player
+-- import Game
 import Data.Char
 import Data.Maybe
 import System.Environment
@@ -66,17 +66,38 @@ playNewGame n = do
 	putStrLn $ "\n The objective of the game is not to be the player to find the bomb"
 	putStrLn $ "\n The bomb is hidden in the field. Guess a number between 0 and" ++ (show (maxNum - 1)) ++ " to begin"
 	let (inTargetNumber, newGen) = next n 
-	let target = mod inTargetNumber maxNum
-	guessFor target 0
-	showBomb target
+	let bomb = mod inTargetNumber maxNum
+	guessFor bomb 0 
+	showBomb bomb
 	again <- playAgain
 	if again 
 		then playNewGame newGen
 		else quitPlaying
 
 -- guessFor handles all the guessing
+-- takes in two Int arguments: one for the 
+-- guess and one as a counter for the attempts
 guessFor :: Int -> Int -> IO ()
+guessFor bomb count = do
+	putStr "Choose a number? "
+	guess <- getNum "\n Curentt guess?"
+	if bomb == guess
+		then foundBomb $ attempts + 1
+		else missedBomb bomb attempts guess 
 
+
+-- foundBomb is what happens when you find the bomb
+-- not good :( 
+foundBomb :: Int -> IO ()
+foundBomb count = do
+	putStrLn "BOOM!*(#@! You found the bomb."
+	putStrLn $ "You died in " ++ show count ++ " turns."
+
+-- missedBomb lets the players keep guessing
+{-- TODO: handle changing the array of possible moves
+--}
+missedBomb bomb attempts guess = do
+	guessFor bomb $ attempts + 1
 
 
 
