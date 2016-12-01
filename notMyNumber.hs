f448575671c5e4a33c38fa9c644a06dfea5c82e4
Hyojin Yi <hyojinyi@live.ca>
2016-12-01 09:05:07 -0800

    choosing who's turn it is and show whose turn it is

--- notMyNumber.hs
+++ notMyNumber.hs
@@ -21,6 +21,19 @@ type PlayerScore = (String, Int, Int)
 setPlayerScore :: String -> Int -> Int -> PlayerScore
 setPlayerScore name win lose = (name, win, lose)
 
+getPlayerName :: PlayerScore -> String
+getPlayerName (name, win, loss) = name 
+
+getPlayerWin :: PlayerScore -> Int 
+getPlayerWin (name, win, loss) = win 
+
+getPlayerLoss :: PlayerScore -> Int
+getPlayerLoss (name, win, loss) = loss 
+
+
+
+
+
 -- Tournament State is Player1 score, Player2 score
 type TournammentState = (PlayerScore, PlayerScore)   -- wins, losses
 
@@ -99,10 +112,22 @@ playNewGame p1 p2 range n = do
     let (inTargetNumber, newGen) = next n 
     let bomb = mod inTargetNumber ((snd range)+1)
     let gameState = setGameState range bomb
-    putStrLn "Who starts? 0=you, 1=computer, 2=exit."
+    
+    putStrLn $ "Who starts? 0 = " ++ (show (getPlayerName p1)) ++ ", 1 = computer, 2 = exit."
     starter <- getLine
-    guessFor bomb 0 range
+    ---- player1 starts
+    --if ((read line :: Int) == 0)
+    --    then guessFor p1 p2 bomb 0 0 range
+    ---- computer starts
+    --else if ((read line :: Int) == 1)
+    --    then guessFor p2 p1 bomb 0 0 range
+    ---- quit game
+    --else if ((read line :: Int) == 2) 
+    --    then quitPlaying
+    guessFor p1 p2 bomb 0 0 range
     showBomb bomb
+    putStrLn $ "\nThe Score: " ++ show (getPlayerName p1) ++ " has " ++ show (getPlayerWin p1) ++ " wins and " ++ show (getPlayerLoss p1) ++ " losses."
+    putStrLn $ "\nThe Score: " ++ show (getPlayerName p2) ++ " has " ++ show (getPlayerWin p2) ++ " wins and " ++ show (getPlayerLoss p2) ++ " losses."
     again <- playAgain
     if again 
         then playNewGame p1 p2 range newGen
@@ -113,15 +138,23 @@ playNewGame p1 p2 range n = do
 
 
 -- guessFor handles all the guessing
--- takes in two Int arguments: one for the 
--- guess and one as a counter for the attempts
-guessFor :: Int -> Int -> (Int, Int) -> IO ()
-guessFor bomb count range = do
+-- guessFor takes in:
+-- player1 player2 
+-- bomb location
+-- attempts of player1, attempts of player2
+-- acceptable range
+-- returns IO ()
+guessFor :: PlayerScore -> PlayerScore -> Int -> Int -> Int -> (Int, Int) -> IO ()
+guessFor p1 p2 bomb count_p1 count_p2 range = do
+  whoseTurn p1
   showRange range
   guess <- getNumber "That's not in the range! Guess again:  " range
   if bomb == guess
-    then foundBomb $ count + 1
-    else missedBomb bomb count range guess 
+    then foundBomb p1 $ count_p1 + 1
+    else missedBomb p1 p2 bomb count_p1 count_p2 range guess 
+
+whoseTurn :: PlayerScore -> IO ()
+whoseTurn p1 = putStrLn $ "Whose turn: " ++ show (getPlayerName p1)
 
 -- keeps asking for a number 
 getNumber :: String -> (Int, Int) -> IO Int 
@@ -130,24 +163,36 @@ getNumber promptAgain range =
 
 -- foundBomb is what happens when you find the bomb
 -- not good :( 
-foundBomb :: Int -> IO ()
-foundBomb count = do
+foundBomb :: PlayerScore -> Int -> IO ()
+foundBomb player count = do
+  let name = getPlayerName player
+  let win = getPlayerWin player 
+  let loss  = getPlayerLoss player
+  let new_Player = setPlayerScore name win (loss+1)
   putStrLn "    BOOM  *&@&!^#&@!*(#@!!    You found the bomb."
-  putStrLn $ "You died in " ++ show count ++ " turns."
+  putStrLn $ show name ++ " died in " ++ show count ++ " turns."
 
 -- missedBomb lets the players keep guessing
 {-- TODO: handle changing the array of possible moves
 !!!
 The too low or too high is temporary for testing only
+missedBomb p1 p2 bomb count range guess 
+
+-- guessFor takes in:
+-- player1 player2 
+-- bomb location
+-- attempts of player1, attempts of player2
+-- acceptable range
+-- returns IO ()
 --}
-missedBomb bomb attempts (lower, upper) guess = do
+missedBomb p1 p2 bomb count_p1 count_p2 (lower, upper) guess = do
   if bomb > guess
       then do 
         putStrLn "too low"
-        guessFor bomb (attempts + 1) (guess, upper)
+        guessFor p2 p1 bomb count_p2 (count_p1 + 1) (guess, upper)
         else do 
           putStrLn "too high"
-          guessFor bomb (attempts + 1) (lower, guess)
+          guessFor p2 p1 bomb count_p2 (count_p1 + 1) (lower, guess)
 
 
 ---------------------------------------------------------------------------
